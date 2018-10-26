f5082592718398f711a3be1fc578a4320c8325ec
Rashed Sarwar <rashed.sarwar@ukaea.uk>
2018-10-26 17:24:03 +0100

    dts update

--- output_files/sof_to_rbf.sh
+++ output_files/sof_to_rbf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-%QUARTUS_ROOTDIR%\bin\quartus_cpf -c -o bitstream_compression=on DE10_NANO_SOC_FB.sof soc_system.rbf
+quartus_cpf -c -o bitstream_compression=on DE10_NANO_SOC_FB.sof soc_system.rbf
 
